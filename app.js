3dcb79f4e11404f515ca6396643ef28b93d1bc6a
Mohamed Igamane <mohamedigamane1203@gmail.com>
2024-09-05 03:09:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const OpenAI = require("openai");
-
+ 
 const app = express();
 
 const SHOP_URL = process.env.SHOP_URL;
